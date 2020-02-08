ac2a2ee84e85af020080f1a289c29ce04f18c31b
harsh <harshithauriti@gmail.com>
2020-02-08 14:42:56 +0530

    first commit

---
+++ Second-week/src/main/java/ClassB.java
@@ -0,0 +1,5 @@
+public abstract class ClassB {
+	
+		public abstract int totalweight(int a[],int n);
+		public abstract int numofcandy(int low,int high,int a[],int n);
+}
---
+++ Second-week/src/test/java/Epam_Submission/Second_week/AppTest.java
@@ -0,0 +1,20 @@
+package Epam_Submission.Second_week;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
